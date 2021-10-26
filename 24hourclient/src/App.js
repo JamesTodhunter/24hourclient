46240690974a3fdbb607c3129e6417fc9458ead8
Ethan Collignon <89707534+ethan-collignon@users.noreply.github.com>
2021-10-25 21:31:07 -0400

    ticketmaster code.nonworking

--- 24hourclient/src/App.js
+++ 24hourclient/src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import TicketMaster from './components/Ticketmaster';
 
-function App() {
+
+
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  localStorage.setItem('longitude', lon);
+  localStorage.setItem('latitude', lat)
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus('Geolocation is not supported by your browser');
+    } else {
+      setStatus('Locating...');
+      navigator.geolocation.getCurrentPosition((position) => {
+        setStatus(null);
+        setLat(position.coords.latitude);
+        setLon(position.coords.longitude);
+      }, () => {
+        setStatus('Unable to retrieve your location');
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <button onClick={getLocation}>Get Location</button>
+      <h1>Coordinates</h1>
+      <p>{status}</p>
+      {lat && <p>Latitude: {lat}</p>}
+      {lon && <p>Longitude: {lon}</p>}
+      <TicketMaster lat={lat} lon={lon}/>
     </div>
   );
+
 }
 
 export default App;
---
+++ 24hourclient/src/components/Ticketmaster.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react'
+
+
+const TicketMaster = ({lat, lon}) => {
+    const [events, setEvents] = useState([])
+    const [Lat, setLat] = useState(lat | 39.78)
+    const [Lon, setLon] = useState(lon | -86.30)
+
+    useEffect(() => {
+
+        const URL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=EHnbpJwE9fhM1FdYk7kY9JpQi0I1MG0h"
+
+        console.log(URL)
+        fetch(URL)
+            .then(res => res.json())
+            .then(json => {
+
+                let myArr = json.page.map(page => {
+                    return {
+                        page: page.page,
+
+                    }
+                })
+            }) 
+            .catch(() => console.error("Failed to get TicketMaster API Fetch Failed"))
+
+    }, [])
+
+    return (
+        <div className='main'>
+            <ul 
+            id='event-name'>
+            </ul>
+            <br />
+        </div>
+    )
+}
+
+export default TicketMaster
