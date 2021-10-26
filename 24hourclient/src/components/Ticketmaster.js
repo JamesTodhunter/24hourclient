5c531870bbf03d5a26c2f7baca141f4083195e61
Ethan Collignon <89707534+ethan-collignon@users.noreply.github.com>
2021-10-25 21:45:27 -0400

    ticketmaster edit

--- 24hourclient/src/components/Ticketmaster.js
+++ 24hourclient/src/components/Ticketmaster.js
@@ -5,10 +5,11 @@ const TicketMaster = ({lat, lon}) => {
     const [events, setEvents] = useState([])
     const [Lat, setLat] = useState(lat | 39.78)
     const [Lon, setLon] = useState(lon | -86.30)
+    const Geohash = 'dp4dp'
 
     useEffect(() => {
 
-        const URL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=EHnbpJwE9fhM1FdYk7kY9JpQi0I1MG0h"
+        const URL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=EHnbpJwE9fhM1FdYk7kY9JpQi0I1MG0h${Geohash}`
 
         console.log(URL)
         fetch(URL)
