d8f83ba3811b72147217caf100543eb08138fd59
Eli Gabovitch <elirgabovitch@gmail.com>
2021-10-26 09:24:48 -0400

    eli

--- 24hourclient/src/App.js
+++ 24hourclient/src/App.js
@@ -1,48 +1,48 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-import Nasa from './Components/Nasa';
-import Weather from './Components/Weather'
+// import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import './App.css';
+// import Nasa from './Components/Nasa';
+// import Weather from './Components/Weather'
 
-const App = () => {
-  const [lat, setLat] = useState(null);
-  const [lon, setLon] = useState(null);
-  const [status, setStatus] = useState(null);
+// const App = () => {
+//   const [lat, setLat] = useState(null);
+//   const [lon, setLon] = useState(null);
+//   const [status, setStatus] = useState(null);
 
-  localStorage.setItem('latitude', lat)
-  localStorage.setItem('longitude', lon)
-
-
-  const getLocation = () => {
-    if (!navigator.geolocation) {
-      setStatus('Geolocation is not supported by your browser');
-    } else {
-      setStatus('Locating...');
-      navigator.geolocation.getCurrentPosition((position) => {
-        setStatus(null);
-        setLat(position.coords.latitude);
-        setLon(position.coords.longitude);
-      }, () => {
-        setStatus('Unable to retrieve your location');
-      });
-    }
-  }
-
-  // localStorage.setItem({getLocation})
+//   localStorage.setItem('latitude', lat)
+//   localStorage.setItem('longitude', lon)
 
 
+//   const getLocation = () => {
+//     if (!navigator.geolocation) {
+//       setStatus('Geolocation is not supported by your browser');
+//     } else {
+//       setStatus('Locating...');
+//       navigator.geolocation.getCurrentPosition((position) => {
+//         setStatus(null);
+//         setLat(position.coords.latitude);
+//         setLon(position.coords.longitude);
+//       }, () => {
+//         setStatus('Unable to retrieve your location');
+//       });
+//     }
+//   }
 
-  return (
-    <div className="App">
-      <button onClick={getLocation}>Get Location</button>
-      <h1>Coordinates</h1>
-      <p>{status}</p>
-      {lat && <p>Latitude: {lat}</p>}
-      {lon && <p>Longitude: {lon}</p>}
-      <Nasa lat={lat} lon={lon}/>
-      <Weather lat={lat} lon={lon}/>
-    </div>
-  );
-}
+//   // localStorage.setItem({getLocation})
 
-export default App;
+
+
+//   return (
+//     <div className="App">
+//       <button onClick={getLocation}>Get Location</button>
+//       <h1>Coordinates</h1>
+//       <p>{status}</p>
+//       {lat && <p>Latitude: {lat}</p>}
+//       {lon && <p>Longitude: {lon}</p>}
+//       <Nasa lat={lat} lon={lon}/>
+//       <Weather lat={lat} lon={lon}/>
+//     </div>
+//   );
+// }
+
+// export default App;
